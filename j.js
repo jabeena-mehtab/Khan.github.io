bfa0e50d35e66aed1c9ba3e55a91a7ef6d2a4036
jabeena-mehtab <45209842+jabeena-mehtab@users.noreply.github.com>
2018-12-15 15:37:03 +0400

    Add files via upload

---
+++ j.js
@@ -0,0 +1,6 @@
+ function g () {
+ 	// body...
+const	get=document.getElementById('P').innerHTML.value;
+  get;  
+ }
+ 
---
+++ java.js
@@ -0,0 +1,109 @@
+var get= document.forms["form"]["P"].value;
+get = "hi" 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function entry() {
+	// body...
+var code=prompt("enter the code first please.");
+if (code==="453676") 
+{  
+  alert("Hi!Mano Welcome to the 0-1 skols");    
+}
+else if (code==="564645") 
+{  
+  alert("Hi!Moiz Welcome to the 0-1 skols");    
+}
+else if (code==="333444") 
+{  
+  alert("Hi!Ayesha, Welcome to the 0-1 skols");    
+}else if (code==="131313") 
+{  
+  alert("Hi!Shahzad, Welcome to the 0-1 skols");    
+}
+else
+{
+	confirm("sorry you entered wrong code.would you like to reenter.")
+      {
+    	if (true) 
+    	{
+    		var code=prompt("Wrong code!would you like to reenter your code.");
+if (code==="453676") 
+{  
+  alert("Hi!Mano Welcome to the 0-1 skols");    
+}
+else if (code==="564645") 
+{  
+  alert("Hi!Moiz Welcome to the 0-1 skols");    
+}
+
+else if (code==="333444") 
+{  
+  alert("Hi!Ayesha, Welcome to the 0-1 skols");    
+}else if (code==="131313") 
+{  
+  alert("Hi!Shahzad, Welcome to the 0-1 skols");    
+}
+else{
+confirm("sorry you entered wrong code.would you like to reenter.")
+      {
+    	if (true) 
+    	{
+    		var code=prompt("⚠Alert! if you enter wrong code again your account will block .");
+ if (code==="453676") 
+  {  
+   alert("Hi!Mano Welcome to the 0-1 skols");    
+  }
+else if (code==="564645") 
+{  
+  alert("Hi!Moiz Welcome to the 0-1 skols");    
+}
+
+else if (code==="333444") 
+{  
+  alert("Hi!Ayesha, Welcome to the 0-1 skols");    
+}else if (code==="131313") 
+{  
+  alert("Hi!Shahzad, Welcome to the 0-1 skols");    
+}
+else{
+             return(entry())
+    }
+    	}
+      }
+
+     }
+    	}
+      }
+       }
+
+}
+
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
